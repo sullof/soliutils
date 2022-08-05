a44962207db8be01e8d5f43d5674a976a01f4230
Francesco Sullo <sullof@gmail.com>
2022-08-05 11:47:37 -0700

    Minor fix to remove warnings

--- contracts/ERC721Receiver.sol
+++ contracts/ERC721Receiver.sol
@@ -1,18 +1,14 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.11;
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721ReceiverUpgradeable.sol";
 
 contract ERC721Receiver is IERC721ReceiverUpgradeable {
   function onERC721Received(
-    // solhint-disable-next-line
-    address operator,
-    // solhint-disable-next-line
-    address from,
-    // solhint-disable-next-line
-    uint256 tokenId,
-    // solhint-disable-next-line
-    bytes calldata data
+    address,
+    address,
+    uint256,
+    bytes calldata
   ) public pure override returns (bytes4) {
     return this.onERC721Received.selector;
   }
